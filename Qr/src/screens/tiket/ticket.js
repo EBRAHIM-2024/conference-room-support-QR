85853182a32161704e2a2999d9159fd9dccc8852
EBRAHIM-2024 <il0xi2019@gmail.com>
2023-10-17 08:18:34 +0300

    tiket

--- Qr/src/screens/tiket/ticket.js
+++ Qr/src/screens/tiket/ticket.js
@@ -109,7 +109,7 @@ function TicketComponent() {
       console.log(err);
      });
 
-      fetch('http://localhost:5000/api/employee/sendSMS',{
+      fetch('https://conference-room-support-app-server.onrender.com/api/employee/sendSMS',{
         method: 'POST',
          headers: {
         'Content-Type': 'application/json'
