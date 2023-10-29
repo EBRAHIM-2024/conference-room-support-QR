7a3073c215339d0b8023325a4d7f3cb4646abaef
EBRAHIM-2024 <il0xi2019@gmail.com>
2023-10-29 10:06:30 +0300

    first update daialog

--- Qr/src/screens/av/av.js
+++ Qr/src/screens/av/av.js
@@ -9,7 +9,7 @@ function AVComponent() {
   return (
 <div className="App">
   <div>
- <iframe src="https://webgl0.s3.eu-west-2.amazonaws.com/Audio+Visual+System+Guide.pdf" width="100%" height="500px" />
+ <iframe src="https://webgl0.s3.eu-west-2.amazonaws.com/Audio+Visual+System+Guide.pdf" target="_blank" width="100%" height="500px" />
  </div>
   {/* <object data="https://webgl0.s3.eu-west-2.amazonaws.com/Audio+Visual+System+Guide.pdf" type="application/pdf" width="100%" height="1200px">
     <p><a href="https://webgl0.s3.eu-west-2.amazonaws.com/Audio+Visual+System+Guide.pdf">You can click here to download the PDF file.</a></p>
